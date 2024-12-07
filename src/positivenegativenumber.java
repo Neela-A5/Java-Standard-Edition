a1aae204b4d31dd8d466f270bd4a1b4d0c10eabc
Neela-A5 <agdihumnabad123@gmail.com>
2024-12-07 23:10:37 +0530

    loop practise day 04

--- src/positivenegativenumber.java
+++ src/positivenegativenumber.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class positivenegativenumber {
     public static void main(String[] args) {
         int num;
-
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
         num = sc.nextInt();
@@ -19,6 +18,20 @@ public class positivenegativenumber {
         {
             System.out.println("The number is zero.");
         }
-    }
 
+        // we can code with another method
+        if(num > 0 ){
+            System.out.println("The number is positive.");
+        }
+        else if(num < 0){
+            System.out.println("The number is negative");
+        }
+        else{
+            System.out.println("The number is zero.");
+        }
+    }
 }
+
+
+
+
