6eb14adb6b2dcaf9e8c2e1ad5f8686bac784aa4e
Neela-A5 <agdihumnabad123@gmail.com>
2025-02-04 15:54:40 +0530

    loop practise day 07

---
+++ src/basicPattern.java
@@ -0,0 +1,33 @@
+public class basicPattern {
+    public static void main(String[] args) {
+        System.out.println("printing 4 rows and 4 columns using Asterisk (*)");
+        for(int i = 1; i <= 4; i++) {
+            for(int j = 1; j<= 4; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("printing 4 rows and 4 columns using Numbers.");
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 4; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("printing 4 rows and 4 columns using Numbers.");
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 4; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
+
+
+
